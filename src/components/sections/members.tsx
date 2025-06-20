e1a17073d5541fd89dac4d938e2bab1ce95455c9
Samuk <sammy.pico.duran@gmail.com>
2025-06-20 11:12:58 -0500

    Fix: Mobile Nav now working

--- src/components/sections/members.tsx
+++ src/components/sections/members.tsx
@@ -110,7 +110,7 @@ export function Members() {
     };
 
     return (
-        <div className="space-y-8 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+        <div id="members" className="space-y-8 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
             <div className="text-center space-y-4 mb-10">
                 <h1>
                     Nuestro Equipo
