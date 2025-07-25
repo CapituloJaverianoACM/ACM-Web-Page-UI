e1bfcddf63e462bd7f4de0e7755fe80bc837f3f5
Adrian Ruiz <adrianruiz0208@gmail.com>
2025-07-25 17:53:07 -0500

    Add: league page structure and dynamic main-navbar. The fix to the cursor problem is on #41.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,19 @@ import { Activities } from "@/components/home/sections/activities";
 import { Hero } from "@/components/home/sections/hero";
 import { Members } from "@/components/home/sections/members";
 import { HeroUIProvider } from "@heroui/react";
-import MainNavbar from '@/components/home/main-navbar';
+import MainNavbar from '@/components/shared/main-navbar';
+
+const navLinks = [
+  { key: "home", label: "Home", href: "#home" },
+  { key: "about us", label: "About Us", href: "#about-us" },
+  { key: "members", label: "Members", href: "#members" },
+  { key: "league", label: "League", href: "/league" },
+];
 
 export default function HomePage() {
   return (
     <HeroUIProvider>
-      <MainNavbar />
+      <MainNavbar navLinks={navLinks} />
       <div>
         <Hero />
         <AboutUs />
