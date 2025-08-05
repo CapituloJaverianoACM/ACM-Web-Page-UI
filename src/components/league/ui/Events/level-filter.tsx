0d1cb70697c26d1a07cc5266d57efd4cd5a3f935
Achalogy <achalogy@gmail.com>
2025-08-05 11:19:58 -0500

    fix enum values

--- src/components/league/ui/Events/level-filter.tsx
+++ src/components/league/ui/Events/level-filter.tsx
@@ -4,17 +4,17 @@ export const LevelFilter = ({
   filter,
   setFilter
 }: {
-  filter: "initial" | "all" | "advanced",
-  setFilter: Dispatch<SetStateAction<"initial" | "all" | "advanced">>
+  filter: "Initial" | "all" | "Advanced",
+  setFilter: Dispatch<SetStateAction<"Initial" | "all" | "Advanced">>
 }) => {
 
   const texts = {
-    "initial": "Inicial",
+    "Initial": "Inicial",
     "all": "Todos",
-    "advanced": "Avanzado"
+    "Advanced": "Avanzado"
   }
 
-  const LevelButton = ({ f }: { f: "initial" | "all" | "advanced" }) => {
+  const LevelButton = ({ f }: { f: "Initial" | "all" | "Advanced" }) => {
     return <div onClick={() => setFilter(f)} className={`glassmorphic transition-all duration-300 ease-in-out select-none ${filter == f ? "shadow-md" : ""} px-2 p-1`}>
       <p className="m-0">{texts[f]}</p>
     </div>
@@ -23,8 +23,8 @@ export const LevelFilter = ({
 
   return <div className="flex gap-2 items-center glassmorphic p-2">
     <LevelButton f={"all"} />
-    <LevelButton f={"initial"} />
-    <LevelButton f={"advanced"} />
+    <LevelButton f={"Initial"} />
+    <LevelButton f={"Advanced"} />
   </div>
 
 }
