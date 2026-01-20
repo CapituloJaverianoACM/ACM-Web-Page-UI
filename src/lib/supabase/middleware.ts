35727d7b2c3a073285c455fc06b28bf93d5ee155
Adrian Ruiz <111095677+adrianrrruiz@users.noreply.github.com>
2026-01-20 00:08:28 -0500

    New: Production and development envs (#142)

--- src/lib/supabase/middleware.ts
+++ src/lib/supabase/middleware.ts
@@ -1,3 +1,4 @@
+import { SUPABASE_KEY, SUPABASE_URL } from "@/config/env";
 import { createServerClient } from "@supabase/ssr";
 import { NextResponse, type NextRequest } from "next/server";
 
@@ -6,39 +7,35 @@ export async function updateSession(request: NextRequest) {
     request,
   });
 
-  const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
-    {
-      cookies: {
-        getAll() {
-          return request.cookies.getAll();
-        },
-        setAll(cookiesToSet) {
-          cookiesToSet.forEach(({ name, value, options }) =>
-            request.cookies.set({
-              name,
-              value,
-              httpOnly: true,
-              ...options,
-            }),
-          );
+  const supabase = createServerClient(SUPABASE_URL!, SUPABASE_KEY!, {
+    cookies: {
+      getAll() {
+        return request.cookies.getAll();
+      },
+      setAll(cookiesToSet) {
+        cookiesToSet.forEach(({ name, value, options }) =>
+          request.cookies.set({
+            name,
+            value,
+            httpOnly: true,
+            ...options,
+          }),
+        );
 
-          supabaseResponse = NextResponse.next({
-            request,
-          });
-          cookiesToSet.forEach(({ name, value, options }) =>
-            supabaseResponse.cookies.set({
-              name,
-              value,
-              httpOnly: true,
-              ...options,
-            }),
-          );
-        },
+        supabaseResponse = NextResponse.next({
+          request,
+        });
+        cookiesToSet.forEach(({ name, value, options }) =>
+          supabaseResponse.cookies.set({
+            name,
+            value,
+            httpOnly: true,
+            ...options,
+          }),
+        );
       },
     },
-  );
+  });
 
   // Do not run code between createServerClient and
   // supabase.auth.getUser(). A simple mistake could make it very hard to debug
